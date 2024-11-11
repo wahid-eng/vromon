14754ef253385393d601fa6c24270932e48bb81a
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-12 02:20:02 +0600

    high level components init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import './App.css'
+import './App.css';
+import Brands from './components/Brands/Brands';
+import Destinations from './components/Destinations/Destination';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Newsletter from './components/Newsletter/Newsletter';
+import Services from './components/Services/Services';
+import Testimonials from './components/Testimonitals/Testimonials';
 
 function App() {
-  return (
-      <div>App</div>
-  )
+	return (
+		<>
+			<Header />
+			<Hero />
+			<Brands />
+			<Services />
+			<Destinations />
+			<Features />
+			<Testimonials />
+			<Newsletter />
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/Destinations/Destination.tsx
@@ -0,0 +1,3 @@
+export default function Destinations() {
+	return <div>Destinations</div>;
+}

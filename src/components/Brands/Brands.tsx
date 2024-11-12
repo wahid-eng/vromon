b311e6041a52980f8dd3789c4221f0d5b117afd1
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-11-12 18:12:38 +0600

    brands section added

--- src/components/Brands/Brands.tsx
+++ src/components/Brands/Brands.tsx
@@ -1,3 +1,19 @@
+import brand1 from '../../assets/images/brand-1.svg';
+import brand2 from '../../assets/images/brand-2.svg';
+import brand3 from '../../assets/images/brand-3.svg';
+import brand4 from '../../assets/images/brand-4.svg';
+import brand5 from '../../assets/images/brand-5.svg';
+
 export default function Brands() {
-	return <div>Brands</div>;
+	return (
+		<section>
+			<div className="container 2xl:max-w-screen-xl py-20 flex flex-wrap gap-8 justify-center lg:justify-between">
+				<img src={brand1} alt="Tripadvisor" className="w-32 h-12" />
+				<img src={brand2} alt="Tripadvisor" className="w-32 h-12" />
+				<img src={brand3} alt="Tripadvisor" className="w-32 h-12" />
+				<img src={brand4} alt="Tripadvisor" className="w-32 h-12" />
+				<img src={brand5} alt="Tripadvisor" className="w-32 h-12" />
+			</div>
+		</section>
+	);
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Button from '../Button/Button';
 export default function Header() {
 	return (
 		<header>
-			<div className="bg-[#FF5722] w-[500px] h-[500px] absolute -translate-x-1/2 -translate-y-1/3 rounded-full opacity-50 blur-[500px] -z-10"></div>
+			<div className="bg-[#FF5722] w-[500px] h-[500px] absolute inset-0 -translate-x-1/2 -translate-y-1/3 rounded-full opacity-50 blur-[500px] -z-10"></div>
 			<div className="container 2xl:max-w-screen-xl py-8 flex items-center justify-between">
 				<a href="#">
 					<figure>
